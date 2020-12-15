d19ed910efd59907231e69b567202606855585d0
Charles Stainer <cstainer@gmu.edu>
2020-12-15 00:51:02 +0000

    Final project complete.

---
+++ final/src/del_robot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosservice call gazebo/delete_model $1
---
+++ final/src/spawn_robot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosrun gazebo_ros spawn_model -file /home/user/catkin_ws/src/final/models/4x4.sdf -sdf -model $1
